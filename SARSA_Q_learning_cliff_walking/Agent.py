b7af6ef0912f19a7785badada9bfc0b29be0de5d
James Cadman <cadman@roe.ac.uk>
2021-11-04 14:00:15 +0000

    Both Q learning and SARSA now working, with some useful plots to show the final state value functions, as well as the routes taken by the agents.

--- SARSA_Q_learning_cliff_walking/Agent.py
+++ SARSA_Q_learning_cliff_walking/Agent.py
@@ -6,7 +6,7 @@ class Agent():
     Class to play the game, choosing which actions to take, and calculating cumulative rewards.
     """
 
-    def __init__(self, epsilon=0.2, alpha=0.5, gamma=1.0):
+    def __init__(self, epsilon=0.5, alpha=0.1, gamma=1.0):
         self.cliff = Cliff()
         self.actions = ["up","down","left","right"]
         self.epsilon = epsilon
@@ -93,26 +93,34 @@ class Agent():
             # take the current state_action_value to be the max over all actions in the current state
             state_action_value = max(self.state_actions[s].values())
 
-    def play(self, episodes=1000):
+    def play(self, episodes=5000, strategy="Q"):
         """
         Execute the agent playing the game
         """
         for N in range(episodes):
             while 1:
                 state = self.pos
+                current_reward = self.cliff.give_reward()
                 # choose action, move agent, and calculate the reward
                 next_action = self.choose_action()
-                next_pos = self.cliff.move_agent(next_action)
-                next_reward = self.cliff.give_reward()
+                self.cliff.move_agent(next_action)
                 self.pos = self.cliff.pos
                 # save positions, actions and rewards history
-                self.states.append([state, next_action, next_reward])
-                print(self.states[-1])
+                self.states.append([state, next_action, current_reward])
                 # check if game has ended
                 if self.cliff.end:
                     break
+
             # game has ended, update estimes of state-action combinations
-            self.update_estimates_Q()
+            if strategy=="Q":
+                self.update_estimates_Q()
+            elif strategy=="SARSA":
+                self.update_estimates_SARSA()
             if N < episodes - 1:
                 # reset and go again - if on the final episode then don't reset, for debugging purposes.
                 self.reset()
+                # also, lets decay epsilon at each timestep
+                self.epsilon = self.epsilon/1.01
+            else:
+                # append the final state to the self.states list, for plotting purposes
+                self.states.append([self.pos, None, None])
--- SARSA_Q_learning_cliff_walking/SARSA_Q_learning_cliff_walking.py
+++ SARSA_Q_learning_cliff_walking/SARSA_Q_learning_cliff_walking.py
@@ -7,11 +7,62 @@ Compare the policies which are obtained from SARSA and Q learning, and evaluate 
 algorithm reaches the policy that it does.
 """
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 from Agent import Agent
+from Cliff import Cliff
+
+def plot_route(states):
+    board = Cliff().board
+    rows = np.shape(board)[0]
+    cols = np.shape(board)[1]
+    route = np.zeros([rows,cols])
+
+    positions_passed = [s[0] for s in states]
+
+    for i in range(rows):
+        for j in range(cols):
+            if (i,j) in positions_passed:
+                route[i,j] = 1
+            elif board[i,j] == "*":
+                route[i,j] = -1
+    plt.figure()
+    plt.imshow(route)
+
+def plot_state_values(states_action_values,strategy):
+    board = Cliff().board
+    rows = np.shape(board)[0]
+    cols = np.shape(board)[1]
+    values = np.zeros([rows,cols])
+
+    for i in range(rows):
+        for j in range(cols):
+            # take the state value as the mean over all values if considering SARSA, and max if considering Q
+            if strategy=="Q":
+                values[i,j] = np.max([states_action_values[i,j][a] for a in Agent().actions])
+            elif strategy=="SARSA":
+                values[i,j] = np.max([states_action_values[i,j][a] for a in Agent().actions])
+    plt.figure()
+    plt.imshow(values)
+    plt.colorbar()
 
 agent = Agent()
-agent.play()
+agent.play(strategy="Q")
+final_route = agent.states
+states_action_values = agent.state_actions
+plot_route(final_route)
+plt.title("Q learning final route")
+plot_state_values(states_action_values, strategy="Q")
+plt.title("Q learning state values")
 
-import pdb ; pdb.set_trace()
+agent = Agent()
+agent.play(strategy="SARSA")
+final_route = agent.states
+states_action_values = agent.state_actions
+plot_route(final_route)
+plt.title("SARSA learning final route")
+plot_state_values(states_action_values, strategy="SARSA")
+plt.title("SARSA learning state values")
 
-print()
+plt.show()
