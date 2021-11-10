12ecd59a670587d12970cc911f75d841bb6f0f57
James Cadman <cadman@roe.ac.uk>
2021-11-10 17:21:12 +0000

    Checkpointing for the day, still a bit of a mess but slowly coming together

--- mountain_car/func_approx_mountain_car.py
+++ mountain_car/func_approx_mountain_car.py
@@ -1,46 +1,68 @@
 import gym
 import numpy as np
 
-env = gym.make('MountainCar-v0')
-env.reset()
+import tiles3 as tiles
 
+class MountainCar():
+    def __init__(self, iht, maxsize, num_tilings):
+        self.env = gym.make('MountainCar-v0')
+        self.iht = iht
+        self.maxsize = maxsize
+        self.num_tilings = num_tilings
+        # reset the car
+        self.state = self.env.reset()
+        # initialise weights
+        self.weights = np.zeros([env.action_space.n, self.maxsize])
+        # hyperparameters
+        self.alpha = 0.1 # learning rate
+        self.epsilon = 0.1 # for epsilon-greedy policy
 
-nweights = 400
-weights = np.zeros([env.action_space.n, nweights])
+    def reset(self):
+        self.state = self.env.reset()
 
-# hyperparameters
-alpha = 0.1 # learning rate
-epsilon = 0.1 # for epsilon-greedy policy
-nepisodes = 1000
+    def scaleToTiles(self, x, y):
+        """
+        Scale our observation space to the number of x,y tiles used, then return
+            the tile that our state is currently in.
+        """
+        high = self.env.observation_space.high
+        low = self.env.observation_space.low
+        scaleFactor = np.sqrt(self.maxsize)/(high-low)
+        return tiles.tiles(self.iht, self.num_tilings, [scaleFactor*x, scaleFactor*y])
 
+    def update_weights(self, Qpi, Qw):
+        TD_loss = Qpi - Qw
+        dw = np.dot(TD_loss,state)
+        dw = -alpha*d
+        return weights + dw
+
+    def calc_Q(self):
+        value = np.dot(self.weights.T)
+        return value
+
+    def policy(self, tiles):
+        """
+        Epsilon-greedy
+        """
+        # for action in [0,1,2]
+        # calc_Q for each action and take the action with the max Q
+        max_reward = -999
+        next_action = None
+        for action in range(self.env.action_space):
+            value = calc_Q(weights[tiles], )
+            if value > max_reward:
+                next_action = action
+                max_reward = value
+        ALSO NEED TO IMPLEMENT EPSILON PART
+        return action
+
+"""
 # position and velocity
 env.observation_space
 
 # get random action
 # 0 = force left, 1 = nothing, 2 = force right
 env.action_space.sample()
-
-def calc_Q(weights, x):
-    value = np.dot(weights, x)
-    return value
-
-def policy(state, weights, epsilon):
-    """
-    Epsilon-greedy
-    """
-    # for action in [0,1,2]
-    # calc_Q for each action and take the action with the max Q
-    """
-    current_max = -999
-    current_action = None
-    for action in env.action_space:
-        value = calc_Q(weights[action], x)
-        if value > current_max:
-            current_action = action
-            current_max = value
-    ALSO NEED TO IMPLEMENT EPSILON PART
-    return action
-    """
 
 # state is x, y position
 state = env.reset()
@@ -49,11 +71,22 @@ for i in range(1000):
     env.render()
     new_state, reward, done, info = env.step(env.action_space.sample())
 env.close()
+"""
 
+"""
+Set up tile code
+"""
+maxsize = 1024 # will give a 32x32 grid
+# set up index hash table
+iht = tiles.IHT(maxsize)
+num_tilings = 8 # number of offset tilings
+
+# initialise the MountainCar class
+MC = MountainCar(iht, maxsize, num_tilings)
+
+nepisodes = 1000
 for N in range(nepisodes):
     # reset environment
-    state = env.reset()
-    """
-    while not terminal:
-        
-    """
+    MC.reset()
+    while True:
+        action = MC.policy
